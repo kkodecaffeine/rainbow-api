29d2cdfd160360b3ca20d3356e41d23d753539f1
kkodecaffeine <kkodecaffeine@gmail.com>
2020-11-07 21:56:41 +0900

    Add IsNullOrEmpty

---
+++ RainbowApp.Core/Entities/BaseEntity.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+namespace RainbowApp.Core.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            if (GetType().IsSubclassOf(typeof(BaseEntity)))
+            {
+                var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                foreach (var property in properties)
+                {
+                    // Get only string properties
+                    if (property.PropertyType != typeof(string))
+                    {
+                        continue;
+                    }
+
+                    if (!property.CanWrite || !property.CanRead)
+                    {
+                        continue;
+                    }
+
+                    if (property.GetGetMethod(false) == null)
+                    {
+                        continue;
+                    }
+                    if (property.GetSetMethod(false) == null)
+                    {
+                        continue;
+                    }
+
+                    if (string.IsNullOrEmpty((string)property.GetValue(this, null)))
+                    {
+                        property.SetValue(this, string.Empty, null);
+                    }
+                }
+            }
+        }
+    }
+}
