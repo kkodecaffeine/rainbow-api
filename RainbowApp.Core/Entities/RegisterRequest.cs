da2db3e4dfc30118f32d436cb4db5bfe567d2c9f
kkodecaffeine <kkodecaffeine@gmail.com>
2020-11-15 19:15:25 +0900

    Update User entity

---
+++ RainbowApp.Core/Entities/RegisterRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RainbowApp.Core.Entities
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string MailAddr { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Range(typeof(bool), "true", "true")]
+        public bool AcceptTerms { get; set; }
+    }
+}
--- RainbowApp.Core/Entities/User.cs
+++ RainbowApp.Core/Entities/User.cs
@@ -8,10 +8,26 @@ namespace RainbowApp.Core.Entities
         public int UserId { get; set; }
         public string Name { get; set; }
         public string MailAddr { get; set; }
+
         [JsonIgnore]
         public string Password { get; set; }
+        public DateTime? PasswordResetYmd { get; set; }
+        [JsonIgnore]
+        public string VerificationToken { get; set; }
+        public DateTime? VerifiedYmd { get; set; }
+        public bool? IsVerifed { get; set; }
+        public string ResetToken { get; set; }
+        public DateTime? ResetTokenExpiredYmd { get; set; }
         public string Domain { get; set; }
         public DateTime CreatedYmd { get; set; }
-        public DateTime? ChangedYmd { get; set; }
+        public DateTime? ChagedYmd { get; set; }
+        //public int UserId { get; set; }
+        //public string Name { get; set; }
+        //public string MailAddr { get; set; }
+        //[JsonIgnore]
+        //public string Password { get; set; }
+        //public string Domain { get; set; }
+        //public DateTime CreatedYmd { get; set; }
+        //public DateTime? ChangedYmd { get; set; }
     }
 }
