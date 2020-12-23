b322ef628e95935d137d68aa3f0bca3a899cbca4
kkodecaffeine <kkodecaffeine@gmail.com>
2020-12-23 22:25:19 +0900

    FIX Change base controll

--- RainbowApp.Api/Controllers/ApiController.cs
+++ RainbowApp.Api/Controllers/ApiController.cs
@@ -7,7 +7,7 @@ namespace RainbowApp.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ApiController : ControllerBase
+    public class ApiController : Controller
     {
         private IMediator _mediator;
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
