759da0cae0c190e94f8063110107d6e5019ec85e
kkodecaffeine <kkodecaffeine@gmail.com>
2020-12-25 09:47:52 +0900

    FIX scripts

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -1,11 +1,3 @@
-# if [ -d /home/ubuntu/deploy ]; then
-#     rm -rf /home/ubuntu/deploy
-# fi
-# 만약 /home/ubuntu/build 디렉토리가 존재하면 지운다는 의미입니다.
-
-# mkdir -vp /home/ubuntu/deploy
-# 다시 새로운 /home/ubuntu/build 디렉토리를 생성합니다.
-
 docker stop rainbow-api
 docker rm rainbow-api
 # 저는 Docker를 이용할 것이기 때문에, 돌가가고 있는 Docker Container를 중지시키고, 제거합니다.
