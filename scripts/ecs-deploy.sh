997878022cc6f850b87464f422a6dc645361ab13
kkodecaffeine <kkodecaffeine@gmail.com>
2020-12-26 16:33:55 +0900

    test commit

--- scripts/ecs-deploy.sh
+++ scripts/ecs-deploy.sh
@@ -3,7 +3,6 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 
 echo "Deploying $TRAVIS_BRANCH on $CLUSTER_NAME"
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $AWS_ECR_ACCOUNT.dkr.ecr.ap-northeast-2.amazonaws.com/$APP_NAME:$environment
-ecs-deploy -t 180 -d $TASK_DEFINITION_API -i $AWS_ECR_ACCOUNT.dkr.ecr.ap-northeast-2.amazonaws.com/$APP_NAME:$environment
 
 else
 echo "Skipping deploy because it's not an allowed branch"
