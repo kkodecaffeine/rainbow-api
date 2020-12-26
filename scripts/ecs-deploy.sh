29f6186995ad51a6614b3e5dcf2d3c8ec72c1938
kkodecaffeine <kkodecaffeine@gmail.com>
2020-12-26 17:39:41 +0900

    FIX ecs-deploy scripts

--- scripts/ecs-deploy.sh
+++ scripts/ecs-deploy.sh
@@ -9,6 +9,11 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 echo "Deploying $TRAVIS_BRANCH on $CLUSTER_NAME"
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $AWS_ECR_ACCOUNT.dkr.ecr.ap-northeast-2.amazonaws.com/$APP_NAME:$environment
 
+TASK_ID=`aws ecs list-tasks -c $CLUSTER_NAME --desired-status RUNNING --family tskdef-rainbow-api | egrep "task" | tr "/" " " | tr "[" " " |  awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition tskdef-rainbow-api | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+aws ecs stop-task -c $CLUSTER_NAME --task ${TASK_ID}
+aws ecs update-service -c $CLUSTER_NAME --service $SERVICE_NAME --task-definition tskdef-rainbow-api:${TASK_REVISION} --desired-count 1
+
 else
 echo "Skipping deploy because it's not an allowed branch"
 fi
