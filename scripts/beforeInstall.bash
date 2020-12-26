3a01077223187bc9ea4c6ae07989f0c3f0684c56
kkodecaffeine <kkodecaffeine@gmail.com>
2020-12-26 14:40:19 +0900

    test commit

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -1,7 +1,6 @@
 docker stop rainbow-api
 docker rm rainbow-api
 # 이후 afterinstall.bash 파일에서 새롭게 받아온 파일을 사용하여 다시 Docker Container를 띄울 예정입니다.
-# [Your Docker Container Name] 예시) woomin-facebook-codedeploy
 
 #docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='[Your DockerHub ID]/[Your Repository Name]:[Your version]')
 docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}')
