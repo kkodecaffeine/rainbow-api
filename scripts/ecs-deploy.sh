95ef26b0c103e53852b8a02cfa261c708c70858e
kkodecaffeine <kkodecaffeine@gmail.com>
2020-12-27 20:39:58 +0900

    FIX change logic process

--- scripts/ecs-deploy.sh
+++ scripts/ecs-deploy.sh
@@ -7,16 +7,15 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 # --run-task                   Run created task now. If you set this, service-name are not needed.
 
 echo "Deploying $TRAVIS_BRANCH on $CLUSTER_NAME"
-ecs-deploy -t 20 -c $CLUSTER_NAME -n $SERVICE_NAME -i $AWS_ECR_ACCOUNT.dkr.ecr.ap-northeast-2.amazonaws.com/$APP_NAME:$environment
 
 OLD_TASK_ID=`aws ecs list-tasks --cluster $CLUSTER_NAME --desired-status RUNNING --family $TASK_DEFINITION_API | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/"`
 CUR_REVISION=$(aws ecs describe-task-definition --task-definition $TASK_DEFINITION_API --query 'taskDefinition.revision' --output text)
-NXT_REVISION=`aws ecs describe-task-definition --task-definition $TASK_DEFINITION_API | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 
 aws ecs stop-task --cluster $CLUSTER_NAME --task ${OLD_TASK_ID}
-
 aws ecs deregister-task-definition --task-definition "$TASK_DEFINITION_API:${CUR_REVISION}"
 
+ecs-deploy -t 20 -c $CLUSTER_NAME -n $SERVICE_NAME -i $AWS_ECR_ACCOUNT.dkr.ecr.ap-northeast-2.amazonaws.com/$APP_NAME:$environment
+NXT_REVISION=`aws ecs describe-task-definition --task-definition $TASK_DEFINITION_API | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --force-new-deployment --task-definition $TASK_DEFINITION_API:${NXT_REVISION} --desired-count 1
 
 else
